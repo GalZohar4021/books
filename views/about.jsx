4ed31b3a78550e66427c8554f027db2e783bb135
GalZohar4021 <118897719+GalZohar4021@users.noreply.github.com>
2023-01-05 14:50:43 +0200

    about page corrected mistake in card text

--- views/about.jsx
+++ views/about.jsx
@@ -5,7 +5,7 @@ export function About() {
         {
             name: 'Gal Zohar',
             title: 'Fullstack Developer',
-            txt: 'I\'m a 27 from Oranit, Israel. I am a junior developer who learn Fullstack development at Coding Academy. My tools: JS including React,Angular and jQuery, HTML, CSS, C#, Java and SQL. I love diving, hiking and have a good time with may pets. I am looking to learn and grow as developer.',
+            txt: 'I\'m a 27 from Oranit, Israel. I am a junior developer who learn Fullstack development at Coding Academy. My tools: Javascript including React,Angular, Node.js and jQuery, HTML, CSS, C#, Java and SQL. I love diving, hiking and have a good time with my pets. I am looking to learn and grow as developer.',
             img: 'assets/img/dev/gal.jpg',
             contact: {
                 facebook: 'https://www.facebook.com/profile.php?id=100077018276598',
